2650999a3a5b9503aa15b7832463f20a5d237e7f
habibayman <habibaayman2004@outlook.com>
2026-08-05 17:19:43 +0300

    feat(match): add Match Interaction static rendering

--- kolibri/plugins/qti_viewer/frontend/components/AssessmentItem.vue
+++ kolibri/plugins/qti_viewer/frontend/components/AssessmentItem.vue
@@ -37,6 +37,8 @@
   import InlineChoice from './interactions/InlineChoice.vue';
   import AssociateInteraction from './interactions/AssociateInteraction.vue';
   import SimpleAssociableChoice from './interactions/SimpleAssociableChoice.vue';
+  import MatchInteraction from './interactions/MatchInteraction.vue';
+  import SimpleMatchSet from './interactions/SimpleMatchSet.vue';
 
   const $themeTokens = themeTokens();
 
@@ -50,6 +52,8 @@
     [InlineChoice.tag]: InlineChoice,
     [AssociateInteraction.tag]: AssociateInteraction,
     [SimpleAssociableChoice.tag]: SimpleAssociableChoice,
+    [MatchInteraction.tag]: MatchInteraction,
+    [SimpleMatchSet.tag]: SimpleMatchSet,
   });
 
   /** @typedef {import('../utils/qti/values.js').QTIValue} QTIValue */
